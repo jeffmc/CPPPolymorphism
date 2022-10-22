8b6fba0404cf9e3a22c30c10da8e0801313aef84
jeffmc <10823453+jeffmc@users.noreply.github.com>
2022-10-22 14:06:04 -0700

    modified:   main.cpp  	modified:   types.h

--- main.cpp
+++ main.cpp
@@ -2,17 +2,31 @@
 // CPP Classes assignment
 
 #include <iostream>
+#include <vector>
 
 #include "types.h"
 
 int main() {
-	
-	Videogame vg = Videogame("Super Mario 64", 1999, "Nintendo", 5.0f);
-	
-	Music mu = Music("Hip Hop", 2000, "Dead Prez", "Loud Record LLC");
 
-	Movie mo = Movie("Top Gun", 2022, "Bill Gates", Duration(0, 2,11,0), 4.13f);
+	std::vector<Media*> mediavec = {};
 
+	mediavec.push_back(
+		new Videogame("Super Mario 64", 1999, "Nintendo", 5.0f)
+	);
+	mediavec.push_back(
+		new Music("Hip Hop", 2000, "Dead Prez", { 0,3,34 },  "Loud Records LLC")
+	);
+	mediavec.push_back(
+		new Music("Easily", 1999, "RHC Peppers", { 0,3,51 },  "Warner Records Inc.")
+	);
+	mediavec.push_back(
+		new Movie("Top Gun", 2022, "Joseph Kosinski", { 2,11,0 }, 4.13f)
+	);
+	for (std::vector<Media*>::const_iterator it = mediavec.cbegin();
+			it != mediavec.cend(); ++it)
+	{
+		(*it)->print();
+	}
 	return 0;
 }
 
--- types.h
+++ types.h
@@ -2,17 +2,14 @@
 
 #include <cstring>
 #include <iostream>
+#include <cstdio>
 
 // Day:Hour:Min:Sec
 typedef unsigned char uchar;
-struct Duration {
-	
-	unsigned long days; // 0-infinity
+struct Duration {	
 	uchar hours;
 	uchar mins;
 	uchar secs; // 0-23, 0-59, 0-59
-	Duration(unsigned long days, uchar hours, uchar mins, uchar secs)
-		: days(days), hours(hours), mins(mins), secs(secs) { };
 };
 struct Date {
 	unsigned long year; //0-infinity (sorry BC)
@@ -22,10 +19,11 @@ typedef const char*const const_cstr;
 typedef unsigned int uint;
 #define ALLOCCPY(arg) this->arg = new char[strlen(arg)+1]; strcpy(this->arg, arg); 
 class Media {
-	private:
+	protected:
 		char* title; // pointer to title of this media. (Owned by this class)
 		uint year; // year in which media was released
 	public:
+		virtual void print() const =0;
 		Media(const_cstr& title, const uint& year) {
 			ALLOCCPY(title);
 			this->year = year;
@@ -34,11 +32,15 @@ class Media {
 			delete[] this->title;
 		}
 };
-class Videogame : private Media {
+class Videogame : public Media {
 	private:
 		char* publisher;
 		float rating;
 	public:
+		void print() const override {
+			printf("%15s %4d %15s %-20.2f\n",
+					this->title,this->year,this->publisher,this->rating);
+		}
 		Videogame(const_cstr& title, const uint& year,
 				const_cstr publisher, const float& rating) 
 			: Media(title, year)
@@ -50,29 +52,42 @@ class Videogame : private Media {
 			delete[] this->publisher;
 		}
 };
-class Music : private Media {
+class Music : public Media {
 	private:
 		char* artist, *publisher;
 		Duration duration;
 	public:
+		void print() const override {
+			printf("%15s %4d %15s %-20s %2d:%2d:%2d\n",
+					this->title,this->year,this->artist,
+					this->publisher,
+					this->duration.hours,this->duration.mins,this->duration.secs);
+		}
 		Music(const_cstr& title, const uint& year,
-				const_cstr& artist, const_cstr& publisher)
+				const_cstr& artist, const Duration& duration, const_cstr& publisher)
 			: Media(title,year)
 		{	
 			ALLOCCPY(artist);
 			ALLOCCPY(publisher);
+			this->duration = duration;
 		}
 		~Music() {
 			delete[] this->artist;
 			delete[] this->publisher;
 		}
 };
-class Movie : private Media {
+class Movie : public Media {
 	private:
 		char* director;
 		Duration duration;
 		float rating;
 	public:
+		void print() const override {
+			printf("%15s %4d %15s %-20.2f %2d:%2d:%2d\n",
+					this->title,this->year,this->director,
+					this->rating,
+					this->duration.hours,this->duration.mins,this->duration.secs);
+		}
 		Movie(const_cstr& title, const uint& year,
 				const_cstr& director, const Duration& duration, const float& rating)
 			: Media(title, year)
