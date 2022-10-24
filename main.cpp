5ad55602a2c1a6e4c5f652dd78ed4d5e67bf97f2
jeffmc <10823453+jeffmc@users.noreply.github.com>
2022-10-24 00:35:40 -0700

    modified:   main.cpp 	modified:   types.h

--- main.cpp
+++ main.cpp
@@ -8,24 +8,36 @@
 
 int main() {
 
-	std::vector<Media*> mediavec = {};
-
-	mediavec.push_back(
-		new Videogame("Super Mario 64", 1999, "Nintendo", 5.0f)
-	);
-	mediavec.push_back(
-		new Music("Hip Hop", 2000, "Dead Prez", { 0,3,34 },  "Loud Records LLC")
-	);
-	mediavec.push_back(
-		new Music("Easily", 1999, "RHC Peppers", { 0,3,51 },  "Warner Records Inc.")
-	);
-	mediavec.push_back(
+	std::vector<Media*> mediavec = {
+		new Videogame("Super Mario 64", 1999, "Nintendo", 5.0f),
+		new Music("Hip Hop", 2000, "Dead Prez", { 0,3,34 },  "Loud Records LLC"),
+		new Music("Easily", 1999, "RHC Peppers", { 0,3,51 },  "Warner Records Inc."),
 		new Movie("Top Gun", 2022, "Joseph Kosinski", { 2,11,0 }, 4.13f)
-	);
+	};
+
+	const int BUFSZ = 16;
+	char buf[BUFSZ] = {};
 	for (std::vector<Media*>::const_iterator it = mediavec.cbegin();
 			it != mediavec.cend(); ++it)
 	{
-		(*it)->print();
+		(*it)->printm0(buf,BUFSZ);
+		printf("%*s",BUFSZ,buf);
+		(*it)->printm1(buf,BUFSZ);
+		printf("%*s",BUFSZ,buf);
+		(*it)->printm2(buf,BUFSZ);
+		printf("%*s",BUFSZ,buf);
+		(*it)->printm3(buf,BUFSZ);
+		printf("%*s",BUFSZ,buf);
+		(*it)->printm4(buf,BUFSZ);
+		printf("%*s",BUFSZ,buf);
+		(*it)->printm5(buf,BUFSZ);
+		printf("%*s",BUFSZ,buf);
+		(*it)->printm6(buf,BUFSZ);
+		printf("%*s",BUFSZ,buf);
+		(*it)->printm7(buf,BUFSZ);
+		printf("%*s",BUFSZ,buf);
+
+		printf("\n");
 	}
 	return 0;
 }
--- types.h
+++ types.h
@@ -15,88 +15,144 @@ struct Date {
 	unsigned long year; //0-infinity (sorry BC)
 	unsigned char days, month; // 1-31, 1-11
 };
+
 typedef const char*const const_cstr;
 typedef unsigned int uint;
-#define ALLOCCPY(arg) this->arg = new char[strlen(arg)+1]; strcpy(this->arg, arg); 
+
+#define ALLOCCPY(mem,arg) this->mem = new char[strlen(arg)+1]; strcpy(this->mem, arg); 
+
+int memPrint(char* buf, const size_t& n, const float& mem) { return snprintf(buf,n,"%f", mem);	}
+int memPrint(char* buf, const size_t& n, const char* mem) { return snprintf(buf,n,"%s", mem); }
+int memPrint(char* buf, const size_t& n, const uint& mem) { return snprintf(buf,n,"%u", mem); }
+int memPrint(char* buf, const size_t& n, const Duration& mem) { return snprintf(buf,n,"%u:%u:%u", mem.hours, mem.mins, mem.secs); }
+
+#define VIRTMEMACC(NUM) virtual int printm##NUM(char* buf, const size_t& n) const =0;
+#define V(n) VIRTMEMACC(n);
+#define VIRTMEMACC2(n0,n1) 					 V(n0);V(n1);
+#define VIRTMEMACC3(n0,n1,n2)				 V(n0);V(n1);V(n2);
+#define VIRTMEMACC4(n0,n1,n2,n3) 			 V(n0);V(n1);V(n2);V(n3);
+#define VIRTMEMACC5(n0,n1,n2,n3,n4) 		 V(n0);V(n1);V(n2);V(n3);V(n4);
+#define VIRTMEMACC6(n0,n1,n2,n3,n4,n5) 		 V(n0);V(n1);V(n2);V(n3);V(n4);V(n5);
+#define VIRTMEMACC7(n0,n1,n2,n3,n4,n5,n6) 	 V(n0);V(n1);V(n2);V(n3);V(n4);V(n5);V(n6);
+#define VIRTMEMACC8(n0,n1,n2,n3,n4,n5,n6,n7) V(n0);V(n1);V(n2);V(n3);V(n4);V(n5);V(n6);V(n7);
+
+#define MEMACC(NUM,MEM) int printm##NUM(char* buf, const size_t& n) const override { return memPrint(buf, n, this->MEM); }
+#define MEMNULL(NUM)    int printm##NUM(char* buf, const size_t& n) const override { *buf = '\0'; return 0; }
+#define MN(n) MEMNULL(n)
+#define MEMNULL2(n0,n1) MN(n0);MN(n1);
+#define MEMNULL3(n0,n1,n2) MN(n0);MN(n1);MN(n2);
+#define MEMNULL4(n0,n1,n2,n3) MN(n0);MN(n1);MN(n2);MN(n3);
+#define MEMNULL5(n0,n1,n2,n3,n4) MN(n0);MN(n1);MN(n2);MN(n3);MN(n4);
+#define MEMNULL6(n0,n1,n2,n3,n4,n5) MN(n0);MN(n1);MN(n2);MN(n3);MN(n4);MN(n5);
+#define MEMNULL7(n0,n1,n2,n3,n4,n5,n6) MN(n0);MN(n1);MN(n2);MN(n3);MN(n4);MN(n5);MN(n6);
+#define MEMNULL8(n0,n1,n2,n3,n4,n5,n6,n7) MN(n0);MN(n1);MN(n2);MN(n3);MN(n4);MN(n5);MN(n6);MN(n7);
+
+#define GENMEDACC() MEMACC(0,m_Title); MEMACC(1,m_Year);
 class Media {
 	protected:
-		char* title; // pointer to title of this media. (Owned by this class)
-		uint year; // year in which media was released
+		char* m_Title; // pointer to title of this media. (Owned by this class)
+		uint m_Year; // year in which media was released
 	public:
 		virtual void print() const =0;
+
+		VIRTMEMACC8(0,1,2,3,4,5,6,7);
+
 		Media(const_cstr& title, const uint& year) {
-			ALLOCCPY(title);
-			this->year = year;
+			ALLOCCPY(m_Title,title);
+			m_Year = year;
 		}
 		~Media() {
-			delete[] this->title;
+			delete[] m_Title;
 		}
 };
+
 class Videogame : public Media {
 	private:
-		char* publisher;
-		float rating;
+		char* m_Publisher;
+		float m_Rating;
 	public:
 		void print() const override {
 			printf("%15s %4d %15s %-20.2f\n",
-					this->title,this->year,this->publisher,this->rating);
+					m_Title,m_Year,m_Publisher,m_Rating);
 		}
+
+		GENMEDACC();
+		MEMACC(2,m_Publisher);
+		MEMACC(3,m_Rating);
+		MEMNULL4(4,5,6,7);
+
 		Videogame(const_cstr& title, const uint& year,
 				const_cstr publisher, const float& rating) 
 			: Media(title, year)
 		{
-			ALLOCCPY(publisher);
-			this->rating = rating;
+			ALLOCCPY(m_Publisher,publisher);
+			m_Rating = rating;
 		}
 		~Videogame() {
-			delete[] this->publisher;
+			delete[] m_Publisher;
 		}
 };
 class Music : public Media {
-	private:
-		char* artist, *publisher;
-		Duration duration;
-	public:
-		void print() const override {
-			printf("%15s %4d %15s %-20s %2d:%2d:%2d\n",
-					this->title,this->year,this->artist,
-					this->publisher,
-					this->duration.hours,this->duration.mins,this->duration.secs);
-		}
-		Music(const_cstr& title, const uint& year,
-				const_cstr& artist, const Duration& duration, const_cstr& publisher)
-			: Media(title,year)
-		{	
-			ALLOCCPY(artist);
-			ALLOCCPY(publisher);
-			this->duration = duration;
-		}
-		~Music() {
-			delete[] this->artist;
-			delete[] this->publisher;
-		}
+private:
+	char* m_Artist;
+	char* m_Publisher;
+	Duration m_Length;
+public:
+	void print() const override {
+		printf("%15s %4d %15s %-20s %2d:%2d:%2d\n",
+				m_Title,m_Year,m_Artist,m_Publisher,
+				m_Length.hours,m_Length.mins,m_Length.secs);
+	}
+
+	GENMEDACC();
+	MEMACC(2,m_Publisher);
+	MEMACC(3,m_Artist);
+	MEMACC(4,m_Length);
+	MEMNULL3(5,6,7);	
+
+	Music(const_cstr& title, const uint& year,
+			const_cstr& artist, const Duration& length, const_cstr& publisher)
+		: Media(title,year)
+	{	
+		ALLOCCPY(m_Artist,artist);
+		ALLOCCPY(m_Publisher,publisher);
+		m_Length = length;
+	}
+	~Music() {
+		delete[] m_Artist;
+		delete[] m_Publisher;
+	}
 };
 class Movie : public Media {
-	private:
-		char* director;
-		Duration duration;
-		float rating;
-	public:
-		void print() const override {
-			printf("%15s %4d %15s %-20.2f %2d:%2d:%2d\n",
-					this->title,this->year,this->director,
-					this->rating,
-					this->duration.hours,this->duration.mins,this->duration.secs);
-		}
-		Movie(const_cstr& title, const uint& year,
-				const_cstr& director, const Duration& duration, const float& rating)
-			: Media(title, year)
-		{
-			ALLOCCPY(director);
-			this->duration = duration;
-			this->rating = rating;
-		}
-		~Movie() {
-			delete[] director;
-		}
+private:
+	char* m_Director;
+	Duration m_Length;
+	float m_Rating;
+public:
+	void print() const override {
+		printf("%15s %4d %15s %-20.2f %2d:%2d:%2d\n",
+				m_Title,m_Year,m_Director,m_Rating,
+				m_Length.hours,m_Length.mins,m_Length.secs);
+	}
+	
+	GENMEDACC();
+	MEMACC(2,m_Director);
+	MEMACC(3,m_Rating);
+	MEMACC(4,m_Length);
+	MEMNULL3(5,6,7);
+
+	Movie(const_cstr& title, const uint& year,
+			const_cstr& director, const Duration& length, const float& rating)
+		: Media(title, year)
+	{
+		ALLOCCPY(m_Director,director);
+		m_Length = length;
+		m_Rating = rating;
+	}
+	~Movie() {
+		delete[] m_Director;
+	}
 };
+
+#undef V
+#undef MN
